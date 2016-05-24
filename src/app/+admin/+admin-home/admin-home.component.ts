01e2448907d7735b0fb1cecab3bf05643a96d28c
gvdonovan-axio <gdonovan@axiometrics.com>
2016-05-24 16:45:09 -0500

    aaa

--- src/app/+admin/+admin-home/admin-home.component.ts
+++ src/app/+admin/+admin-home/admin-home.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'ax-company',
-    template: require('./company-summary.component.html'),
-    styles: [ require('./company-summary.component.scss') ],
+    template: require('./admin-home.component.html'),
+    styles: [ require('./admin-home.component.scss') ],
     directives: [],
     providers: []
 
 })
-export class CompanySummaryComponent {}
+export class AdminHomeComponent {}
