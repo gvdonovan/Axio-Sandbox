637e11cfefcf01381d5d8bbe3fa257c71b9b76ac
stweedie <skylertweedie@gmail.com>
2016-05-26 23:56:12 +0100

    property sidebar styling

--- src/app/+property/property-sidebar/property-sidebar.component.ts
+++ src/app/+property/property-sidebar/property-sidebar.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'property-sidebar',
   template: require('./property-sidebar.component.html'),
-  styleUrls: [require('./property-sdiebar.component.scss')]
+  // template: `<a class="sidebar-brand m-b-0 sidebar-brand-bg sidebar-brand-border">Axio Workspace</a>`,
+  styles: [require('./property-sdiebar.component.scss')]
 })
 export class PropertySidebarComponent implements OnInit{
   @Input() model: any;
